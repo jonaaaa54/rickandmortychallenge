c9b2ac622bc9fd9f383771cf2b2d7d03e7a01556
jona <creaw15@gmail.com>
2021-03-27 18:00:30 -0300

    add:sparkles: : add service that gets data from api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  
+  // Api data from rickandmortyapi.
+  rickAndMortyApi:{
+    baseUrl: 'https://rickandmortyapi.com/api',
+    characters: 'https://rickandmortyapi.com/api/character',
+    locations: 'https://rickandmortyapi.com/api/location',
+    episodes: 'https://rickandmortyapi.com/api/episode'
+  }
 };
 
 /*
