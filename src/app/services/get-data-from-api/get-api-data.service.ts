19072a02b814df6daadc8be9f5b6e6f1b7c7fe1b
jona <creaw15@gmail.com>
2021-03-27 18:44:38 -0300

    add:package: : add model that stores names.

--- src/app/services/get-data-from-api/get-api-data.service.ts
+++ src/app/services/get-data-from-api/get-api-data.service.ts
@@ -9,7 +9,6 @@ export class GetApiDataService {
 
   constructor(private httpClient: HttpClient) {}
 
-
   getData(restData: string): Observable<any>{
     return this.httpClient.get<any[]>(restData);
   }
